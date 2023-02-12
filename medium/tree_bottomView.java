c1939088d1f8401dc9b23429c0d5e69a22e10e33
Shubham Pandey <shubhampandeylearning@gmail.com>
2023-02-12 15:31:11 +0530

    Tree bottom view TreeMap

--- medium/tree_bottomView.java
+++ medium/tree_bottomView.java
@@ -7,6 +7,40 @@ import java.util.*;
 //https://takeuforward.org/data-structure/bottom-view-of-a-binary-tree/
 public class tree_bottomView {
 
+    public static ArrayList<Integer> bottomViewTreeMap(Node root) {
+        //To keep track of ""latest"" nodes with a given horizontal dist from root.
+        //treemap keeps track of order of insertion. hence it is ordered
+        TreeMap<Integer, Integer> map = new TreeMap<>();//dist, node.data
+        ArrayList<Integer> bv = new ArrayList<>();
+        Queue<Pair<Node, Integer>> q = new LinkedList<>();//to do level order traversal
+        int currDist = 0, parentDist = -1;//horizontal dist
+
+        map.put(0, root.data);
+        q.add(new Pair<>(root, currDist));
+
+        while (!q.isEmpty()) {
+            Pair popped = q.remove();
+            Node poppedNode = (Node) popped.first;
+            parentDist = (int) popped.second;
+
+            if (poppedNode.left != null) {
+                currDist = parentDist - 1;
+                q.add(new Pair<>(poppedNode.left, currDist));
+                map.put(currDist, poppedNode.left.data); // left child have dist - 1;
+            }
+            if (poppedNode.right != null) {
+                currDist = parentDist + 1;
+                q.add(new Pair<>(poppedNode.right, currDist));
+                map.put(currDist, poppedNode.right.data); // left child have dist - 1;
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            bv.add(entry.getValue());
+        }
+        return bv;
+    }
+
     public static ArrayList<Integer> bottomView(Node root) {
         //To keep track of ""latest"" nodes with a given horizontal dist from root.
         HashMap<Integer, Integer> map = new HashMap<>();//dist, node.data
