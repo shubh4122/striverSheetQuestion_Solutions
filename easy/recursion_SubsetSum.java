cc7c951d8c59d14765a8671bd556e0b39e050e27
Shubham Pandey <shubhampandeylearning@gmail.com>
2023-07-17 14:33:34 +0530

    Recursion subset sum, updated with COMMENT explaination

--- easy/recursion_SubsetSum.java
+++ easy/recursion_SubsetSum.java
@@ -24,6 +24,12 @@ public class recursion_SubsetSum {
     //TC : O(2^n), SC : O(2^n) - (coz for each elem, we have 2 options, take or not. hence by permutation - 2^n)
 
     //To understand, might see /DSA/Recursion.java/subsetArr()  Steps fully explained there.
+    /*
+        Hypothesis:  subsetSum -> adds sum of subsets of array 'ip' upto index 'indes'
+        Eg: [5,2,1]
+            fun(ip, 1, sum, ans) -> this call adds the subset sums of arr ip, upto index 1[5,2] in ans
+            hence ans now will be:  [0,2,5,7]
+     */
     public static void subsetSum(ArrayList<Integer> ip, int index, int opSum, ArrayList<Integer> ans) {
         //Ip/Op method. - Recursive tree
         //BC
